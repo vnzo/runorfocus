8167dac8bed9309d069b231f6c713999a7cf7bc0
vnzo <60271290+vnzo@users.noreply.github.com>
2020-09-08 09:12:45 +0800

    Update rof-browser.sh

--- rof-browser.sh
+++ rof-browser.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-#pgrep -i -f $BROWSER >/dev/null && i3-msg [id=(xdotool search --name $BROWSER)] focus >/dev/null || $BROWSER & sleep 0.5 ; pkill -f $0
 pgrep -i -f $BROWSER >/dev/null && i3-msg [class=$BROWSER] focus >/dev/null || $BROWSER & sleep 0.5 ; pkill -f $0
-#pgrep -i -f $BROWSER >/dev/null && i3-msg [class=$BROWSER] focus >/dev/null || $BROWSER & sleep 1 ; killall rof-browser.sh
